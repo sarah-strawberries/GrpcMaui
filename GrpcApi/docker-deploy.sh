b50f6d4a1401c758fa306eda75e6d30a0caea4ce
Sarah <sarah.martin@students.snow.edu>
2025-02-12 16:12:50 -0700

    made a script and a Dockerfile

---
+++ GrpcApi/docker-deploy.sh
@@ -0,0 +1,51 @@
+﻿#!/bin/bash
+
+RESOURCE_GROUP="BadgerClan"
+LOCATION="westus"
+IMAGE="mateotomaszeuski/badgerclan-grpc:1"
+PORT=5001
+REGISTRY_SERVER="index.docker.io"
+REGISTRY_USER="sarahstrawberries"
+REGISTRY_PASS="dckr_pat_7AygjQA7LIslR054dcG-i0ovuzw"
+
+# Create resource group
+az group create --name $RESOURCE_GROUP --location $LOCATION -o none
+
+# Ask user for the number of clients to generate
+read -p "Enter the number of clients to generate: " CLIENT_COUNT
+
+# Generate clients
+for ((i=1; i<=CLIENT_COUNT; i++))
+do
+    CLIENT_NAME="$REGISTRY_USER$i"
+    DNS_LABEL="$CLIENT_NAME"
+    echo "Creating client: $CLIENT_NAME"
+
+    az container create \
+        --resource-group $RESOURCE_GROUP \
+        --name $CLIENT_NAME \
+        --image $IMAGE \
+        --dns-name-label $DNS_LABEL \
+        --os-type Linux \
+        --cpu 1 \
+        --memory 1.5 \
+        --registry-login-server $REGISTRY_SERVER \
+        --registry-username $REGISTRY_USER \
+        --registry-password $REGISTRY_PASS \
+        --ports 5000 5001 \
+        --environment-variables \
+            "Kestrel__EndPoints__gRPC__Url=http://*:5000" \
+            "Kestrel__EndPoints__gRPC__Protocols=Http2" \
+            "Kestrel__EndPoints__Https__Url=http://*:5001" \
+            "Kestrel__EndPoints__Https__Protocols=Http1"\
+        --output none
+
+    echo "Client $CLIENT_NAME created at: http://$DNS_LABEL.$LOCATION.azurecontainer.io:$PORT"
+done
+
+# Allow user to delete the resource group
+read -p "Press 'q' to delete the resource group, or any other key to exit: " DELETE_CONFIRM
+if [ "$DELETE_CONFIRM" == "q" ]; then
+    echo "Deleting resource group $RESOURCE_GROUP..."
+    az group delete --name $RESOURCE_GROUP --yes --no-wait
+fi
