5f1ea4810393664d3d06483113fa283c5571c25a
Sarah <sarah.martin@students.snow.edu>
2025-02-12 15:45:56 -0700

    added internal Kestrel

--- GrpcApi/Program.cs
+++ GrpcApi/Program.cs
@@ -1,8 +1,24 @@
 using GrpcShared;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using ProtoBuf.Grpc.Server;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCodeFirstGrpc();
+
+// Configure kestrel to run using the right ports and protocols
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(5001, listenOptions =>
+    {
+        listenOptions.Protocols = HttpProtocols.Http1;
+    });
+
+    options.ListenAnyIP(5000, listenOptions =>
+    {
+        listenOptions.UseHttps();
+        listenOptions.Protocols = HttpProtocols.Http2;
+    });
+});
 
 var app = builder.Build();
 
